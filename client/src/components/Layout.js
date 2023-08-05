0532989b41eccde2c7a316d64540a682ea7f9d16
Ankit rahi <120669540+Ankit9179@users.noreply.github.com>
2023-08-05 17:29:24 +0530

    first commit

---
+++ client/src/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductsListingPage from '../pages/ProductsListingPage'
+
+const Layout = () => {
+  return (
+    <>
+    <ProductsListingPage />
+    
+    </>
+  )
+}
+
+export default Layout
---
+++ client/src/pages/ProductsListingPage.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react"
+import './ProductsListingPage.css'
+import axios from 'axios'
+
+const ProductsListingPage = () => {
+    const [product, setProduct] = useState([])
+
+    //call get data function for api data
+    useEffect(() => {
+        getData()
+    }, [setProduct])
+
+    //get data function
+    const getData = async () => {
+        try {
+            const res = await axios.get("https://fakestoreapi.com/products")
+            setProduct(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    // console.log(product)
+
+
+    return <>
+        <div className="Product-listing-container">
+            <h1 className="text-center">Products</h1>
+            <div className="Product-card-box">
+
+
+
+                {
+
+
+
+                    product.map((item, index) => (
+
+                        <div className="card  card-content col-sm-6 " key={index} >
+                            <img src={item.image} className="card-img-top" alt="img" />
+                            <div className="card-body">
+                                <h5 className="card-title">{item.title}</h5>
+                                <h2>$ {item.price}</h2>
+
+                                <div className="ctn-rating">
+                                    <button className="btn btn-primary " >More</button>
+                                    <p>★	 {item.rating.rate}</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    ))
+
+
+                }
+
+
+
+
+
+
+            </div>
+        </div>
+    </>;
+};
+
+export default ProductsListingPage;
